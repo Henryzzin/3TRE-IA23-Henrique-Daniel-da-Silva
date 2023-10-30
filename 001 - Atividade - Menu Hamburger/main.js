a95339087d34dadb70c2cd0fdaf0504d7a8810dd
Henryzzin <131017175+Henryzzin@users.noreply.github.com>
2023-10-30 13:48:19 -0300

    Delete 001 - Atividade - Menu Hamburger directory

--- 001 - Atividade - Menu Hamburger/main.js
+++
@@ -1,7 +0,0 @@
-const btHamburger = document.querySelector(".bt-hamburger-toggle")
-const mainMenu = document.querySelector("nav.main-menu")
-
-btHamburger.addEventListener("click", () => {
-    btHamburger.classList.toggle("opened")
-    mainMenu.classList.toggle("opened")   
-})
